d9183078b081967039709f7217eaa70ad3d6aff5
Roman Hangartner <Roman.Hangartner@microsoft.com>
2015-02-20 11:32:35 -0800

    Add DateInput widget to pick the birthdate and return a reactive prediction based on the age and the glucose level.

--- server.R
+++ server.R
@@ -1,10 +1,22 @@
 library(shiny)
 
-diabetesRisk <- function(glucose) glucose / 200
+diabetesRisk <- function(glucose,birthdate) {
+        glucose * ageInDays(birthdate) / 1000
+}
+ageInDays <- function(birthdate) 
+{
+        today <- as.POSIXct(Sys.time(),tz='GMT')
+        bd <- as.POSIXct(birthdate,tz='GMT')        
+        d <- today - bd
+        as.numeric(d, units = "days")
+}
 
 shinyServer(
   function(input, output) {
+    prediction <- reactive({diabetesRisk(input$glucose,input$birthday)})
+    output$dateInput <- renderPrint({input$birthday})
+    output$age <- renderPrint({ageInDays(input$birthday)})
     output$inputValue <- renderPrint({input$glucose})
-    output$prediction <- renderPrint({diabetesRisk(input$glucose)})
+    output$prediction <- renderPrint({prediction()})    
   }
 )
--- ui.R
+++ ui.R
@@ -7,15 +7,19 @@ shinyUI(
     headerPanel("Diabetes prediction"),
   
     sidebarPanel(
-      numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5),
-      submitButton('Submit')
+      dateInput('birthday','Enter your date of birth', value = '1990-01-01', min = '1900-01-01', max = Sys.Date()-1, startview = "year", weekstart = 1),
+      numericInput('glucose', 'Glucose mg/dl', 90, min = 50, max = 200, step = 5)
     ),
     mainPanel(
         h3('Results of prediction'),
         h4('You entered'),
         verbatimTextOutput("inputValue"),
+        h4('and'),
+        verbatimTextOutput("dateInput"),
         h4('Which resulted in a prediction of '),
-        verbatimTextOutput("prediction")
+        verbatimTextOutput("prediction"),
+        h4('Your age in days is'),
+        verbatimTextOutput("age")
     )
   )
 )
